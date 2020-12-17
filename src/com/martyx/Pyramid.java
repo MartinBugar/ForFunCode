d14e1ab05529753776f709c164391b06fb64d42d
prometheus <martin1.bugar@gmail.com>
2020-12-17 12:40:09 +0100

    Pyramid

--- src/com/martyx/Pyramid.java
+++ src/com/martyx/Pyramid.java
@@ -3,18 +3,21 @@ package com.martyx;
 public class Pyramid {
     public static void main(String[] args) {
 
-         int riadky= 10;
-         int pocetHviezdiciek = 1;
+         int riadky= 5;
+
+         //riadky = 5,4,3,2,1
+         //medzery = 4,3,2,1
+         //hviezdicky = 1,3,5
 
 
-
+         int pocetHviezdiciek = 1; //pociatocna hodnota
         for (int i = 1; i <= riadky; i ++){ // posuva riadky
 
             for (int j = riadky - i; j >= 1; j-- ){ // kresli medzeri
                 System.out.print(" ");
             }
 
-            for (int a = 1; a <= pocetHviezdiciek; a++){ // kresli hviezdicky
+            for (int k = 1; k <= pocetHviezdiciek; k++){ // kresli hviezdicky
                 System.out.print("*");
             }
 
